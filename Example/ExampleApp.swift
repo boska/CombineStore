507c98878489e4b6700d8e3c62a47f62529b6a7f
Yang Lee <yang.lee@strv.com>
2020-08-06 13:29:01 +0200

    Add counter example

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Yang on 2020/8/6.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CounterView()
+        }
+    }
+}
